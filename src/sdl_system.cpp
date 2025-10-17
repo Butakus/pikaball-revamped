8f38a912263baf14aab5406755b372d5c45adfb6
Fran <butakus@gmail.com>
2025-10-17 20:28:14 +0200

    Switch to C23 #embed

--- src/sdl_system.cpp
+++ src/sdl_system.cpp
@@ -83,7 +83,9 @@ SDLSystem::~SDLSystem() {
 
 void SDLSystem::load_sprite_sheet() {
   // Load PNG from embedded data or from files. Throw if error.
-  SDL_Surface* sprites_surface = SDL_LoadPNG_IO(load_resource(sprite_sheet_filename), true);
+  // SDL_Surface* sprites_surface = SDL_LoadPNG_IO(load_resource(sprite_sheet_filename), true);
+  SDL_Surface* sprites_surface =
+    SDL_LoadPNG_IO(load_resource(sprite_sheet_filename), true);
 
   if (sprites_surface == nullptr) {
     SDL_Log( "Unable to load image %s! SDL Error: %s\n", sprite_sheet_filename, SDL_GetError());
